f59b6d43a0318181f927d22c0a2aaf003232b415
Laurentiu Ichim <laurentiu.ichim.ro@gmail.com>
2021-12-17 12:46:08 +0200

    local resources (images)
    Working with images

---
+++ WorkingWithResources/WorkingWithResources/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace WorkingWithResources
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void clicked(object sender, EventArgs e)
+        {
+            this.img.Source = this.img.Source.ToString().EndsWith("_1.png") ?
+                "/Resources/Images/jogger_2.png" :
+                "/Resources/Images/jogger_1.png";
+        }
+       
+    }
+}
